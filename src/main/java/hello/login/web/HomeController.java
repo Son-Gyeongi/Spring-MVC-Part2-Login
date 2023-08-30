54e4129832d13cc7a88357cc78097789b341324d
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-30 23:39:41 +0900

    로그인 처리1 - 쿠키 사용 (로그인 및 로그아웃)

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,47 @@
 package hello.login.web;
 
+import hello.login.web.member.Member;
+import hello.login.web.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    // 새로운 홈 화면 - 로그인 처리까지 되는 화면
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId,
+                            Model model) {
+        /*
+            먼저 쿠키를 받는다.
+            스프링에서 제공하는 @CookieValue 애노테이션
+            required = false - 로그인 안 한 사용자도 들어올 수 있다.
+         */
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) { // DB에 없을 경우
+            return "home";
+        }
+        // 로그인 성공한 사용자, 쿠키가 있는 사용자
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -27,7 +29,8 @@ public class LoginController {
 
     // 실제 로그인 처리가 되는 로직
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                        HttpServletResponse response) {
         if (bindingResult.hasErrors()) { // 에러가 있다면
             return "login/loginForm";
         }
@@ -42,8 +45,27 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        // 로그인 성공 처리
+        // 쿠키 만들기 - 쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        // 쿠키 생성한 것을 서버에서 HTTP응답을 보낼때 response에 넣어서 보내야 한다.
+        response.addCookie(idCookie);
 
         return "redirect:/"; // 로그인이 되면 홈으로 보내기
+    }
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        // 쿠키 지우기
+        expireCookie(response, "memberId");
+        return "redirect:/"; // home으로 이동
+    }
+
+    // 쿠키 지우는 방법 - 쿠키 시간을 다 없애면 된다.
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0); // 쿠키가 0이라서 웹 브라우저로 넘어가면 종료가 된다.
+        response.addCookie(cookie);
     }
 }
