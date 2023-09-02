23d3ec1298f0eea52d37a39790637949510aa580
Son-Gyeongi <thsruddl77@gmail.com>
2023-09-02 16:05:16 +0900

    로그인 처리1 - 서블릿 HTTP 세션2 와 TrackingModes (HomeController - homeLoginV3Spring()메서드)

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -66,7 +67,7 @@ public class HomeController {
         return "loginHome";
     }
     // 서블릿 HTTP 세션1
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         // 세션 찾기
@@ -78,6 +79,22 @@ public class HomeController {
 
         // 로그인 시점에 세션에 보관한 회원 객체를 찾는다.
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER); // Object -> Member 캐스팅(형변환)
+
+        // 세션에 회원 데이터가 없으면 home으로 이동
+        if (loginMember == null) { // DB에 없을 경우
+            return "home";
+        }
+
+        // 세션이 유지되면(세션이 유지되고 loginMember데이터가 있는 걸 확인이 되면) 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+    // 서블릿 HTTP 세션2
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model) {
+        // @SessionAttribute - 세션에서 Attribute 한번에 꺼낸다.
 
         // 세션에 회원 데이터가 없으면 home으로 이동
         if (loginMember == null) { // DB에 없을 경우
