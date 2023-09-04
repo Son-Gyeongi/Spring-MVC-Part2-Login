0e0150ad2ef59d9cb3464db5880dc9d0b9990f5b
Son-Gyeongi <thsruddl77@gmail.com>
2023-09-04 23:53:59 +0900

    로그인 처리2 - edit. 서블릿 필터 / 요청 로그 주석 추가 - LogFilter 로그 필터 - WebConfig 필터 설정

--- src/main/java/hello/login/web/filter/LogFilter.java
+++ src/main/java/hello/login/web/filter/LogFilter.java
@@ -20,7 +20,7 @@ public class LogFilter implements Filter {
         // HTTP 요청이 올 때마다 doFilter()가 먼저 호출된다.
         log.info("log filter doFilter");
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request; // 부모 ServletRequest, 자식 HttpServletRequest
+        HttpServletRequest httpRequest = (HttpServletRequest) request; // 부모 ServletRequest, 자식 HttpServletRequest 다운캐스팅
         // 모든 사용자의 요청 URI를 남겨보자
         String requestURI = httpRequest.getRequestURI();
 
@@ -29,7 +29,8 @@ public class LogFilter implements Filter {
 
         try {
             log.info("REQUEST [{}][{}]", uuid, requestURI);
-            chain.doFilter(request, response); // 다음 필터 호출 (다음 필터 있으면 필터 호출, 없으면 서블릿 호출)
+            chain.doFilter(request, response); // 다음 필터 호출 (다음 필터 있으면 필터 호출, 없으면 서블릿 호출), 여기서는 디스패처 서블릿
+            // chain.doFilter() 호출 안 하면 다음 단계로 진행이 안된다. (서블릿, 컨트롤러 호출이 안된다.)
         } catch (Exception e) {
             throw e;
         } finally {
